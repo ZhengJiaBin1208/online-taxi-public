1aabcf0122045a10732172d7cb4d60eba23ecb92
ZhengJiaBin1208 <zhengjiabin@bestseller.com.cn>
2023-08-17 18:10:21 +0800

    1.引用feign

--- api-passenger/src/main/java/com/zjb/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/zjb/apipassenger/ApiPassengerApplication.java
@@ -3,6 +3,7 @@ package com.zjb.apipassenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @ClassName ApiPassengerApplication
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {

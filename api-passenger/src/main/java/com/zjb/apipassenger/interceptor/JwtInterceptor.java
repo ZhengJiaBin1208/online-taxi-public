11ea48c2a988a7f2cd59ada9e810fca649155fa9
ZhengJiaBin1208 <zhengjiabin@bestseller.com.cn>
2023-09-06 11:57:53 +0800

    1.jwt拦截器编写

---
+++ api-passenger/src/main/java/com/zjb/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,75 @@
+package com.zjb.apipassenger.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.zjb.internalcommon.dto.ResponseResult;
+import com.zjb.internalcommon.util.JwtUtils;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * @ClassName JwtInterceptor
+ * @Description TODO
+ * @Author zhengjiabin
+ * @Date 2023/9/6 11:45
+ * @Version 1.0
+ **/
+public class JwtInterceptor implements HandlerInterceptor {
+
+
+    /**
+     * Interception point before the execution of a handler. Called after
+     * HandlerMapping determined an appropriate handler object, but before
+     * HandlerAdapter invokes the handler.
+     * <p>DispatcherServlet processes a handler in an execution chain, consisting
+     * of any number of interceptors, with the handler itself at the end.
+     * With this method, each interceptor can decide to abort the execution chain,
+     * typically sending an HTTP error or writing a custom response.
+     * <p><strong>Note:</strong> special considerations apply for asynchronous
+     * request processing. For more details see
+     * {@link AsyncHandlerInterceptor}.
+     * <p>The default implementation returns {@code true}.
+     *
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @param handler  chosen handler to execute, for type and/or instance evaluation
+     * @return {@code true} if the execution chain should proceed with the
+     * next interceptor or the handler itself. Else, DispatcherServlet assumes
+     * that this interceptor has already dealt with the response itself.
+     * @throws Exception in case of errors
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        boolean result = true;
+        String resultString = "";
+
+        String token = request.getHeader("Authorization");
+        try {
+            JwtUtils.parseToken(token);
+        }catch (SignatureVerificationException e){
+            resultString = "token sign error";
+            result = false;
+        }catch (TokenExpiredException e){
+            resultString = "token time out";
+            result = false;
+        }catch (AlgorithmMismatchException e){
+            resultString = "token AlgorithmMismatchException";
+            result = false;
+        }catch (Exception e){
+            resultString = "token invalid";
+            result = false;
+        }
+
+        if (!result){
+            PrintWriter out = response.getWriter();
+            out.print(JSONObject.parseObject(ResponseResult.fail(resultString).toString()));
+        }
+        return result;
+    }
+}
